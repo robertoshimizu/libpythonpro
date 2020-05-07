ef931a5142e366ddcfd1a83a4a4873fba269a577
robertoshimizu <rshimizubr@gmail.com>
2020-05-07 18:43:20 -0300

    Correcao de Travis

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/media/rober/HDD1/Git_Repo/curso-django/.venv"
+set -gx VIRTUAL_ENV "/media/rober/HDD1/Git_Repo/libpythonpro/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
